7668c1c526748e5adb1748d18f6045c238c49f97
Andrew Wood <me@andrewwood.me>
2020-06-22 12:51:52 -0400

    Fix sg_id and ping_sg_id

--- lib/beaker/hypervisor/aws_sdk.rb
+++ lib/beaker/hypervisor/aws_sdk.rb
@@ -281,6 +281,7 @@ module Beaker
       assoc_pub_ip_addr = host['associate_public_ip_address']
       sg_id = host['sg_id'] || false
       ping_sg_id = host['ping_sg_id'] || false
+      iam_role = host['iam_role'] || false
 
       if vpc_id && !subnet_id
         raise RuntimeError, "A subnet_id must be provided with a vpc_id"
@@ -344,20 +345,15 @@ module Beaker
         end
       end
 
-      # REMOVE DEBUG
-      @logger.notify(ddddddddddddddddd: "#{sg_id}")
       if sg_id
         @logger.notify("aws-sdk: Set security group for instance: #{sg_id}")
-        security_group.group_id = sg_id
-        ping_security_group.group_id = ping_sg_id
+        security_group[group_id] = sg_id
+        ping_security_group[group_id] = ping_sg_id
       else
         security_group = ensure_group(vpc || region, Beaker::EC2Helper.amiports(host), sg_cidr_ips)
         #check if ping is enabled
         ping_security_group = ensure_ping_group(vpc || region, sg_cidr_ips)
       end
-
-      # REMOVE DEBUG
-      @logger.notify("#{security_group}")
 
       msg = "aws-sdk: launching %p on %p using %p/%p%s" %
             [host.name, amitype, amisize, image_type,
